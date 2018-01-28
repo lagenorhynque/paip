b804d68c778aee445f3928ebbb86e06558ede826
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2018-01-29 08:54:47 +0900

    Exercise 1.3-1.5

--- src/paip/chapter01/core.clj
+++ src/paip/chapter01/core.clj
@@ -3,8 +3,31 @@
 
 ;;;; 1. Introduction to Lisp
 
+;; Exercise 1.1
+;; TODO
+
 ;; Exercise 1.2
 (defn power [x n]
   (cond (zero? n) 1
         (even? n) (expt (power x (/ n 2)) 2)
         :else (* x (power x (dec n)))))
+
+;; Exercise 1.3
+(def atom? (complement sequential?))
+
+(defn count-atoms [expr]
+  (cond (atom? expr) 1
+        (empty? expr) 0
+        :else (+ (count-atoms (first expr))
+                 (count-atoms (rest expr)))))
+
+;; Exercise 1.4
+(defn count-anywhere [x expr]
+  (cond (= x expr) 1
+        (atom? expr) 0
+        :else (+ (count-anywhere x (first expr))
+                 (count-anywhere x (next expr)))))
+
+;; Exercise 1.5
+(defn dot-product [xs ys]
+  (apply + (map * xs ys)))
--- test/paip/chapter01/core_test.clj
+++ test/paip/chapter01/core_test.clj
@@ -4,9 +4,28 @@
 
 ;;;; 1. Introduction to Lisp
 
+;; Exercise 1.1
+;; TODO
+
 ;; Exercise 1.2
 (t/deftest power-test
+  (t/is (= 9 (sut/power 3 2)))
   (t/is (= 1 (sut/power 2 0)))
   (t/is (= 2 (sut/power 2 1)))
   (t/is (= 16 (sut/power 2 4)))
   (t/is (= 32 (sut/power 2 5))))
+
+;; Exercise 1.3
+(t/deftest count-atoms-test
+  (t/is (= 3 (sut/count-atoms '(a (b) c))))
+  (t/is (= 0 (sut/count-atoms ())))
+  (t/is (= 2 (sut/count-atoms '(a () c))))
+  (t/is (= 3 (sut/count-atoms '(a nil c)))))
+
+;; Exercise 1.4
+(t/deftest count-anywhere-test
+  (t/is (= 3 (sut/count-anywhere 'a '(a ((a) b) a)))))
+
+;; Exercise 1.5
+(t/deftest dot-product-test
+  (t/is (= 110 (sut/dot-product '(10 20) '(3 4)))))
