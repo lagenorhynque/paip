544b4fa037a762f6148b6e3e330b9e17b47df294
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2018-02-04 00:18:13 +0900

    Chapter 2

--- src/paip/chapter01/core.clj
+++ src/paip/chapter01/core.clj
@@ -3,15 +3,106 @@
 
 ;;;; 1. Introduction to Lisp
 
+(+ 2 2)
+
+(+ 1 2 3 4 5 6 7 8 9 10)
+
+(- (+ 9000 900 90 9) (+ 5000 500 50 5))
+
+;;; 1.1
+
+(concat '(Pat Kim) '(Robin Sandy))
+
+'(Pat Kim)
+
+'John
+'(John Q Public)
+'2
+2
+'(+ 2 2)
+(+ 2 2)
+;; John
+;; (John Q Public)
+
+(concat '(Pat Kim) (list '(John Q Public) 'Sandy))
+(count (concat '(Pat Kim) (list '(John Q Public) 'Sandy)))
+
+;;; 1.2
+
+(def p '(John Q Public))
+p
+(def x 10)
+(+ x x)
+(+ x (count p))
+
+;;; 1.3
+
+;;; 1.4
+
+p
+(first p)
+(rest p)
+(second p)
+(first (nnext p))
+(fnext (nnext p))
+(count p)
+
+(def x' '((first element) 2 (element 3) ((4)) 5))
+(count x')
+(first x')
+(second x')
+(first (nnext x'))
+(fnext (nnext x'))
+(first (fnext (nnext x')))
+(first (nnext (nnext x')))
+(first x')
+(second (first x'))
+
+p
+(cons 'Mr p)
+(cons (first p) (rest p))
+(def town (list 'Anytown 'USA))
+(list p 'of town 'may 'have 'already 'won!)
+(concat p '(of) town '(may have already won!))
+p
+
+(last p)
+
+;;; 1.5
+
 (defn last-name
   "Select the last name from a name represented as a list."
   [name]
   (last name))
 
+(last-name p)
+(last-name '(Rear Admiral Grace Murray Hopper))
+(last-name '(Rex Morgan MD))
+(last-name '(Spot))
+(last-name '(Aristotle))
+
 (defn first-name
   "Select the first name from a name represented as a list."
   [name]
   (first name))
+
+p
+(first-name p)
+(first-name '(Wilma Flintstone))
+(def names '((John Q Public) (Malcolm X)
+                             (Admiral Grace Murray Hopper) (Spot)
+                             (Aristotle) (A A Milne) (Z Z Top)
+                             (Sir Larry Olivier) (Miss Scarlet)))
+(first-name (first names))
+
+;;; 1.6
+
+(map last-name names)
+
+(map - '(1 2 3 4))
+(map + '(1 2 3 4) '(10 20 30 40))
+
+(map first-name names)
 
 (def titles
   "A list of titles that can appear at the start of a name."
@@ -24,14 +115,27 @@
     (first-name' (rest name))
     (first name)))
 
+(map first-name' names)
+(first-name' '(Madam Major General Paula Jones))
+
+;;; 1.7
+
 (defn mappend
   "Apply fn to each element of list and append the results."
   [f coll]
   ;; (apply concat (map f coll))
   (mapcat f coll))
 
-(defn self-and-double [x]
-  (list x (+ x x)))
+(apply + '(1 2 3 4))
+
+(apply concat '((1 2 3) (a b c)))
+
+(defn self-and-double [x] (list x (+ x x)))
+(self-and-double 3)
+(apply self-and-double '(3))
+
+(map self-and-double '(1 10 300))
+(mappend self-and-double '(1 10 300))
 
 (defn number-and-negation
   "If x is a number, return a list of x and -x."
@@ -44,6 +148,8 @@
   [input]
   (mappend number-and-negation input))
 
+(numbers-and-negations '(testing 1 2 3 test))
+
 (defn mappend'
   "Apply fn to each element of list and append the results."
   [f coll]
@@ -51,6 +157,41 @@
     ()
     (concat (f (first coll))
             (mappend' f (rest coll)))))
+
+(+ 2 3)
+(apply + '(2 3))
+;; (+ '(2 3))
+
+((fn [x] (+ x 2)) 4)
+
+(map (fn [x] (+ x x))
+     '(1 2 3 4 5))
+(mappend (fn [l] (list l (reverse l)))
+         '((1 2 3) (a b c)))
+
+;;; 1.8
+
+"a string"
+(count "a string")
+(count "")
+
+;;; 1.9
+
+'John
+(def p' 'John)
+(defn twice [x] (+ x x))
+(if (= 2 3) (throw (RuntimeException.)) (+ 5 6))
+
+(+ 2 3)
+(- (+ 90 9) (+ 50 5 (count '(Pat Kim))))
+
+42
+-273.15
+"a string"
+
+(+ (* 3 4) (* 5 6))
+
+;;; 1.10
 
 ;; Exercise 1.1
 (def suffixes
---
+++ src/paip/chapter02/core.clj
@@ -0,0 +1,194 @@
+(ns paip.chapter02.core)
+
+;;;; 2. A Simple Lisp Program
+
+;;; 2.1
+
+;;; 2.2
+
+(declare one-of)
+
+(defn Article []
+  (one-of '(the a)))
+(defn Noun []
+  (one-of '(man ball woman table)))
+(defn Verb []
+  (one-of '(hit took saw liked)))
+(defn noun-phrase []
+  (concat (Article) (Noun)))
+(defn verb-phrase []
+  (concat (Verb) (noun-phrase)))
+(defn sentence []
+  (concat (noun-phrase) (verb-phrase)))
+
+(defn one-of
+  "Pick one element of set, and make a list of it."
+  [set]
+  (list (rand-nth set)))
+
+(sentence)
+(noun-phrase)
+(verb-phrase)
+
+(declare Adj PP)
+
+(defn Adj* []
+  (if (zero? (rand-int 2))
+    ()
+    (concat (Adj) (Adj*))))
+
+(defn PP* []
+  (if (rand-nth '(true false))
+    (concat (PP) (PP*))))
+
+(defn Adj [] (one-of '(big little blue green adiabatic)))
+(defn Prep [] (one-of '(to in by with on)))
+(defn noun-phrase' [] (concat (Article) (Adj*) (Noun) (PP*)))
+(defn PP [] (concat (Prep) (noun-phrase')))
+
+(defn sentence' []
+  (concat (noun-phrase') (verb-phrase)))
+
+;;; 2.3
+
+(def simple-grammar
+  "A grammar for a trivial subset of English."
+  '((sentence -> (noun-phrase verb-phrase))
+    (noun-phrase -> (Article Noun))
+    (verb-phrase -> (Verb noun-phrase))
+    (Article -> the a)
+    (Noun -> man ball woman table)
+    (Verb -> hit took saw liked)))
+
+(def grammar
+  "The grammar used by generate. Initially, this is
+  simple-grammar, but we can switch to other grammars."
+  (atom simple-grammar))
+
+(defn assoc' [item alist]
+  (some #(when (= (first %) item) %) alist))
+
+(assoc' 'Noun @grammar)
+
+(defn rule-lhs
+  "The left-hand side of a rule."
+  [rule]
+  (first rule))
+
+(defn rule-rhs
+  "The right-hand side of a rule."
+  [rule]
+  (nnext rule))
+
+(defn rewrites
+  "Return a list of the possible rewrites for this category."
+  [category]
+  (rule-rhs (assoc' category @grammar)))
+
+(defn generate
+  "Generate a random sentence or phrase."
+  [phrase]
+  (cond (list? phrase) (mapcat generate phrase)
+        (rewrites phrase) (generate (rand-nth (rewrites phrase)))
+        :else (list phrase)))
+
+(generate 'sentence)
+(generate 'noun-phrase)
+(generate 'verb-phrase)
+
+(defn generate'
+  "Generate a random sentence or phrase."
+  [phrase]
+  (if (list? phrase)
+    (mapcat generate' phrase)
+    (if-let [choices (rewrites phrase)]
+      (generate' (rand-nth choices))
+      (list phrase))))
+
+;; Exercise 2.1
+(defn generate''
+  "Generate a random sentence or phrase."
+  [phrase]
+  (let [choices (atom nil)]
+    (cond (list? phrase) (mapcat generate'' phrase)
+          (reset! choices (rewrites phrase)) (generate'' (rand-nth @choices))
+          :else (list phrase))))
+
+;; Exercise 2.2
+(defn non-terminal? [phrase]
+  (boolean (rewrites phrase)))
+
+(defn generate'''
+  "Generate a random sentence or phrase."
+  [phrase]
+  (cond (list? phrase) (mapcat generate''' phrase)
+        (non-terminal? phrase) (generate''' (rand-nth (rewrites phrase)))
+        :else (list phrase)))
+
+;;; 2.4
+
+;;; 2.5
+
+(def bigger-grammar
+  ""
+  '((sentence -> (noun-phrase verb-phrase))
+    (noun-phrase -> (Article Adj* Noun PP*) (Name) (Pronoun))
+    (verb-phrase -> (Verb noun-phrase PP*))
+    (PP* -> () (PP PP*))
+    (Adj* -> () (Adj Adj*))
+    (PP -> (Prep noun-phrase))
+    (Prep -> to in by with on)
+    (Adj -> big little blue green adiabatic)
+    (Article -> the a)
+    (Name -> Pat Kim Lee Terry Robin)
+    (Noun -> man ball woman table)
+    (Verb -> hit took saw liked)
+    (Pronoun -> he she it these those that)))
+
+(reset! grammar bigger-grammar)
+
+(generate 'sentence)
+
+;;; 2.6
+
+(defn generate-tree
+  "Generate a random sentence or phrase,
+  with a complete parse tree."
+  [phrase]
+  (cond (list? phrase) (map generate-tree phrase)
+        (rewrites phrase) (cons phrase
+                                (generate-tree (rand-nth (rewrites phrase))))
+        :else (list phrase)))
+
+(generate-tree 'sentence)
+
+(defn combine-all
+  "Return a list of lists formed by appending a y to an x.
+  E.g., (combine-all '((a) (b)) '((1) (2)))
+  -> ((a 1) (b 1) (a 2) (b 2))."
+  [xs ys]
+  (mapcat (fn [y]
+            (map (fn [x] (concat x y)) xs))
+          ys))
+
+(reset! grammar simple-grammar)
+
+(defn generate-all
+  "Generate a list of all possible expansions of this phrase."
+  [phrase]
+  (cond (nil? phrase) (list nil)
+        (list? phrase) (combine-all (generate-all (first phrase))
+                                    (generate-all (next phrase)))
+        (rewrites phrase) (mapcat generate-all (rewrites phrase))
+        :else (list (list phrase))))
+
+(generate-all 'Article)
+(generate-all 'Noun)
+(generate-all 'noun-phrase)
+(count (generate-all 'sentence))
+
+;; Exercise 2.3
+;; TODO
+
+;; Exercise 2.4
+;; TODO
