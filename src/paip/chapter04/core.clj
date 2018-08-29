7fbf1a31a51ff8158d53644c6195fe97b852efb5
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2018-08-29 20:46:32 +0900

    test GPS

--- src/paip/chapter04/core.clj
+++ src/paip/chapter04/core.clj
@@ -3,13 +3,13 @@
 
 ;;;; 4. GPS: The General problem Solver
 
-(def state
+(def ^:dynamic *state*
   "The current state: a list of conditions."
-  (atom #{}))
+  #{})
 
-(def ops
+(def ^:dynamic *ops*
   "A list of available operations."
-  (atom #{}))
+  [])
 
 (defrecord Op [action
                preconds
@@ -34,20 +34,44 @@
   "Print a message and update state if op is applicable."
   [op]
   (when (every? achieve (:preconds op))
-    (println "executing" (:action op))
-    (swap! state set/difference (:del-list op))
-    (swap! state set/union (:add-list op))
+    (println (list 'executing (:action op)))
+    (set! *state* (set/difference *state* (:del-list op)))
+    (set! *state* (set/union *state* (:add-list op)))
     true))
 
 (defn achieve
   "A goal is achieved if it already holds,
   or if there is an appropriate op for it that is applicable."
   [goal]
-  (or (contains? @state goal)
+  (or (contains? *state* goal)
       (some apply-op
-            (filter #(appropriate? goal %) @ops))))
+            (filter #(appropriate? goal %) *ops*))))
 
 (defn GPS
   "General Problem Solver: achieve all goals using ops."
   [state goals ops]
-  (when (every? achieve goals) :solved))
+  (binding [*state* state
+            *ops* ops]
+    (when (every? achieve goals) :solved)))
+
+(def school-ops
+  [(make-op :action 'drive-son-to-school
+            :preconds '#{son-at-home car-works}
+            :add-list '#{son-at-school}
+            :del-list '#{son-at-home})
+   (make-op :action 'shop-installs-battery
+            :preconds '#{car-needs-battery shop-knows-problem shop-has-money}
+            :add-list '#{car-works})
+   (make-op :action 'tell-shop-problem
+            :preconds '#{in-communication-with-shop}
+            :add-list '#{shop-knows-problem})
+   (make-op :action 'telephone-shop
+            :preconds '#{know-phone-number}
+            :add-list '#{in-communication-with-shop})
+   (make-op :action 'look-up-number
+            :preconds '#{have-phone-book}
+            :add-list '#{know-phone-number})
+   (make-op :action 'give-shop-money
+            :preconds '#{have-money}
+            :add-list '#{shop-has-money}
+            :del-list '#{have-money})])
